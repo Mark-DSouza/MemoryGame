66ad126293dd85d594f4464062074088af9e88a4
Mark D'Souza <markdsouza434@gmail.com>
2020-05-03 15:46:42 +0530

    Add functions: checkForMatch, disableCards and unflipCards to refactor app.js

--- app.js
+++ app.js
@@ -17,30 +17,48 @@ function flipCard(event) {
         // If first click
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
 
-    else {
-        // If second click
-        hasFlippedCard = false;
-        secondCard = this;
-
-        // // console.log(firstCard.dataset.framework, secondCard.dataset.framework);
-        // console.log(firstCard, secondCard);
-
-
+    // If second click
+    hasFlippedCard = false;
+    secondCard = this;
         // Do the cards match?
+        checkForMatch();
+
+}
+
+function checkForMatch() {
+    const isMatch = (firstCard.dataset.framework === secondCard.dataset.framework);
+    isMatch ? 
+        disableCards() // It's a match!!
+            :
+        unflipCards() // Not a match
+    ;
+    
+    /************************************************************************
         if (firstCard.dataset.framework === secondCard.dataset.framework) {
             // It's a match!!
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
+            disableCards();
         }
         else {
             // Not a match
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500); // Delay to see the flipping
+            unflipCards();
         }
-    }
+    **************************************************************************/
 }
 
+function disableCards() {
+    // Removes event listeners from the cards so that they are disabled
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+
+function unflipCards() {
+    // Removes flip class from the cards so that they un-flip
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500); // Delay to see the flipping
+}
