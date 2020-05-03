6adff4ace77cb73d1cf3a36bee8f0c34932cded5
Mark D'Souza <markdsouza434@gmail.com>
2020-05-03 15:32:09 +0530

    Add functionality to check if flipped cards are the same or not

--- app.js
+++ app.js
@@ -2,10 +2,45 @@
 const cards = document.querySelectorAll('.memory-card');
 
 // Event Listeners
-memoryCard.forEach(card => {card.addEventListener('click', flipCard)});
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+// Variable
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 // Functions
 function flipCard(event) {
-    this.classList.toggle('flip');
+    // Add 'flip' class on click
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        // If first click
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+
+    else {
+        // If second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // // console.log(firstCard.dataset.framework, secondCard.dataset.framework);
+        // console.log(firstCard, secondCard);
+
+
+        // Do the cards match?
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            // It's a match!!
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        }
+        else {
+            // Not a match
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1500); // Delay to see the flipping
+        }
+    }
 }
 
